48dd6827cf8bde3200db8b6ef085f517ea5a1247
G36maid <miku65434@gmail.com>
2025-01-25 17:47:56 +0800

    UPD: combat logic

--- scripts/ally.gd
+++ scripts/ally.gd
@@ -1,5 +1,12 @@
 extends Node2D
 
+const max_health = 100
+var health 
+
+var resource_food 
+var resource_ammo
+var resource_metel
+
 @onready var raycast =  $Area2D/CollisionShape2D/Sprite2D/RayCast2D
 
 var resource_food := 0
@@ -8,11 +15,19 @@ var resource_metal := 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	health = 100;
+	resource_food = 0
+	resource_ammo = 0
+	resource_metel = 0
 	pass # Replace with function body.
-
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if(raycast.is_colliding()):
-		print("has enemy at  front")
+	if (health < max_health && resource_food > 0 ):
+		health += 1
+		resource_food -= 1
+	
+	if(resource_ammo > 0 and raycast.is_colliding() ):
+		var target = raycast.get_collider()
+		print(target)
 	pass
--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,14 +1,18 @@
 extends Node2D
 
+const max_health = 100
+var health 
 
 @onready var ally_parent = $/root/game/allys
 
 var lan
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(lan+1)
+	health = 100
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float):
+	
+	
 	position.y += 5
