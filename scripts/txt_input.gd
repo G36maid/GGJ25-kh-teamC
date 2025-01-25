5f7246dd155c2ec0834a160aceaf2d75f950b72c
noookia8110 <matchman7980@gmail.com>
2025-01-26 03:01:33 +0800

    add error parser

--- scripts/txt_input.gd
+++ scripts/txt_input.gd
@@ -1,13 +1,16 @@
 extends TextEdit
 
-@export var other_text_edit:TextEdit = null
+@export var error_reporter: TextEdit = null
 
-var ret_ptn  = RegEx.new()
+var ret_ptn = RegEx.new()
 var goto_ptn = RegEx.new()
 var grab_ptn = RegEx.new()
 var drop_ptn = RegEx.new()
-var spc_ptn  = RegEx.new()
+var spc_ptn = RegEx.new()
 var highlighter = CodeHighlighter.new()
+
+signal send_invalid_cmd(invalid_cmd)
+signal parse_invalid_cmds
 
 func _ready() -> void:
 	syntax_highlighter = highlighter
@@ -17,9 +20,9 @@ func _ready() -> void:
 	drop_ptn.compile(r"drop\s*(food|ammo|metal)\s*([0-9]+)\s*$")
 	spc_ptn.compile(r"\s+")
 	
-	var actions:PackedStringArray = ["return", "goto", "grab", "drop"]
-	var resources:PackedStringArray = ["food", "ammo", "metal"]
-	var locations:PackedStringArray = ["ally0", "ally1", "ally2", "ally3", "ally4", "hq"]
+	var actions: PackedStringArray = ["return", "goto", "grab", "drop"]
+	var resources: PackedStringArray = ["food", "ammo", "metal"]
+	var locations: PackedStringArray = ["ally0", "ally1", "ally2", "ally3", "ally4", "hq"]
 	for str in actions:
 		highlighter.add_keyword_color(str, Color(0.9, 0.669, 0.657))
 	for str in resources:
@@ -27,6 +30,7 @@ func _ready() -> void:
 	for str in locations:
 		highlighter.add_keyword_color(str, Color(0.795, 0.372, 0.423))
 	highlighter.set_number_color(Color(0.8, 0.644, 0.92))
+	highlighter.set_symbol_color(Color(0.94, 0.824, 0.573))
 	
 	grab_focus()
 
@@ -42,8 +46,9 @@ func _input(event: InputEvent) -> void:
 
 func parse_cmds(new_cmds: String) -> void:
 	var cmds: PackedStringArray = new_cmds.split("\n")
-	var empty_cmd_idx: int = cmds.find("") 
+	var empty_cmd_idx: int = cmds.find("")
 	var parsed_cmds: Array[PackedStringArray]
+
 	while (empty_cmd_idx != -1):
 		print("remove empty string")
 		cmds.remove_at(empty_cmd_idx);
@@ -51,18 +56,22 @@ func parse_cmds(new_cmds: String) -> void:
 
 	for i in cmds.size():
 		if (ret_ptn.search(cmds[i])):
-			print("return at %d"%i)
+			# print("return at %d"%i)
 			parsed_cmds.append(cmds[i].strip_edges().rsplit(" ", true, 0))
 		elif (goto_ptn.search(cmds[i])):
-			print("goto at %d"%i)
+			# print("goto at %d"%i)
 			parsed_cmds.append(spc_ptn.sub(cmds[i].strip_edges(), " ", true).rsplit(" ", true, 1))
 		elif (grab_ptn.search(cmds[i])):
-			print("grab at %d"%i)
+			# print("grab at %d"%i)
 			parsed_cmds.append(spc_ptn.sub(cmds[i].strip_edges(), " ", true).rsplit(" ", true, 2))
 		elif (drop_ptn.search(cmds[i])):
-			print("drop at %d"%i)
+			# print("drop at %d"%i)
 			parsed_cmds.append(spc_ptn.sub(cmds[i].strip_edges(), " ", true).rsplit(" ", true, 2))
 		else:
-			print("no match at %d" % i)
+			send_invalid_cmd.emit(cmds[i])
+			#error_reporter.invalid_cmds.append(cmds[i])
+			# print("no match at %d" % i)
+	print("Valid cmds: ")
 	print(parsed_cmds)
 	$/root/game.send_commands_to_deliverer(parsed_cmds)
+	parse_invalid_cmds.emit()
