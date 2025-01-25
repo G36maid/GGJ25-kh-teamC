4e4c4bf1feb589e611278b149afaf72cb1447954
noookia8110 <matchman7980@gmail.com>
2025-01-25 11:05:20 +0800

    add text input node

---
+++ scripts/txt_input.gd
@@ -0,0 +1,21 @@
+extends TextEdit
+
+var nxtCmd=""
+#onready var other_text_edit = 
+@export var otherTextEdit:TextEdit = null
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+  
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("new_line"):
+		insert_text_at_caret("\n")
+	elif event.is_action_pressed("send_cmd"):
+		nxtCmd = text
+		otherTextEdit.text = nxtCmd
+		clear()
