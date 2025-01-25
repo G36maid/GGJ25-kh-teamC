9039ddcddc19cef81f1af6537f5934ea2ae6e251
G36maid <miku65434@gmail.com>
2025-01-25 17:53:08 +0800

    fix: hotfix

--- scripts/ally.gd
+++ scripts/ally.gd
@@ -3,9 +3,6 @@ extends Node2D
 const max_health = 100
 var health 
 
-var resource_food 
-var resource_ammo
-var resource_metel
 
 @onready var raycast =  $Area2D/CollisionShape2D/Sprite2D/RayCast2D
 
@@ -16,9 +13,6 @@ var resource_metal := 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	health = 100;
-	resource_food = 0
-	resource_ammo = 0
-	resource_metel = 0
 	pass # Replace with function body.
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
