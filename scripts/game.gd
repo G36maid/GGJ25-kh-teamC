310dc5d120506c49dbccf490b85938eb740c9c5c
G36maid <miku65434@gmail.com>
2025-01-26 14:08:52 +0800

    style: update background

--- scripts/game.gd
+++ scripts/game.gd
@@ -32,7 +32,7 @@ var state: State = State.BeforeStart
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	$beforestart.visible=true
 	
 func _start() -> void:
 	$beforestart.visible=false
